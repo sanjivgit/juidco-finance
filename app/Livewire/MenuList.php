89127b6990d40b6e2a13ea69512e42c65580c218
sanjivgit <sanjivkumarhzb1427@gmail.com>
2024-01-12 17:32:18 +0530

    Adding menu list

--- app/Livewire/MenuList.php
+++ app/Livewire/MenuList.php
@@ -7,42 +7,46 @@ use Livewire\Component;
 class MenuList extends Component
 {
 
-  public $sidebarLinks =
-    [
-        'moduleName' => 'Finance Management',
+  public $sidebarLinks = [
+    'moduleName' => 'Finance Management',
+    'path' => '/',
+    'icon' => '/icons/sidebar/finance.svg',
+    'subModules' => [
+      [
+        'moduleName' => 'Masters',
+        'icon' => '/icons/sidebar/masters.svg',
         'path' => '/',
-        'icon' => '/icons/sidebar/finance.svg',
-        'subModules' => [
-            [
-                'moduleName' => 'Masters',
-                'icon' => '/icons/sidebar/masters.svg',
-                'path' => '/',
-                'subModules' => [
-                    ['moduleName' => 'Charts of Account', 'path' => '/'],
-                    ['moduleName' => 'Bank Masters', 'path' => '/bank-master'],
-                    ['moduleName' => 'Vendor Masters', 'path' => '/vendor'],
-                    ['moduleName' => 'Work Order Entry', 'path' => '/workorder'],
-                    ['moduleName' => 'Cheque Book Entry', 'path' => '/chequebook'],
-                ],
-            ],
-            [
-                'moduleName' => 'Documentation & Record Keeping',
-                'icon' => '/icons/sidebar/document.svg',
-                'path' => '/document-and-record',
-                'subModules' => [
-                    ['moduleName' => 'Bank Masters', 'path' => '/'],
-                    ['moduleName' => 'Vendor Masters', 'path' => '/vendor'],
-                    ['moduleName' => 'Work Order Entry', 'path' => '/workorder'],
-                    ['moduleName' => 'Cheque Book Entry', 'path' => '/chequebook'],
-                ],
-            ],
-        ],
-      ];
+        // 'subModules' => [
+        //   ['moduleName' => 'Charts of Account', 'path' => '/'],
+        //   ['moduleName' => 'Bank Masters', 'path' => '/bank-master'],
+        //   ['moduleName' => 'Vendor Masters', 'path' => '/vendor'],
+        //   ['moduleName' => 'Work Order Entry', 'path' => '/workorder'],
+        //   ['moduleName' => 'Cheque Book Entry', 'path' => '/chequebook'],
+        // ],
+      ],
+      [
+        'moduleName' => 'Documentation & Record Keeping',
+        'icon' => '/icons/sidebar/document.svg',
+        'path' => '/document-and-record',
+        // 'subModules' => [
+        //   ['moduleName' => 'Bank Masters', 'path' => '/'],
+        //   ['moduleName' => 'Vendor Masters', 'path' => '/vendor'],
+        //   ['moduleName' => 'Work Order Entry', 'path' => '/workorder'],
+        //   ['moduleName' => 'Cheque Book Entry', 'path' => '/chequebook'],
+        // ],
+      ],
+    ],
+
+
+    // "ABC" => "Abc Aniketb",
+
+    // "subModules" =>["Aniket"]
+  ];
+
+
 
   public function render()
   {
-    $data = json_decode(json_encode($this->sidebarLinks));
-
-    return view('livewire.menu-list',['data' => $data]);
+    return view('livewire.menu-list');
   }
 }
--- resources/views/livewire/menu-list.blade.php
+++ resources/views/livewire/menu-list.blade.php
@@ -1,3 +1,19 @@
 <div>
-    {{-- Stop trying to control. --}}
+    <ul>
+        @foreach ($sidebarLinks as $items)
+                @if (is_array($items))
+                @foreach($items as $i)
+            @if (is_array($i))
+            @foreach($i as $a)
+            <li>{{$a}}</li>
+            @endforeach
+            @else
+            <li>{{$i}}</li>
+            @endif
+            @endforeach
+            @else
+                <li>{{$items}}</li>
+            @endif
+        @endforeach
+    </ul>
 </div>
