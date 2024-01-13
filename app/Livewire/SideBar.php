3420ace6f6d7bbcb28b2f0e770048d9a8aacf15f
ukm3423 <ukm.office3423@gmail.com>
2024-01-13 11:20:33 +0530

    sidebar

--- app/Livewire/SideBar.php
+++ app/Livewire/SideBar.php
@@ -9,34 +9,34 @@ class SideBar extends Component
 
     public $sidebarLinks = [
         'moduleName' => 'Finance Management',
-        'path' => '/home',
+
         'icon' => '/svg/home.svg',
         'subModules' => [
-          [
-            'moduleName' => 'Masters',
-            'icon' => '/svg/home.svg',
-            'path' => '/home',
-            'subModules' => [
-              ['moduleName' => 'Charts of Account', 'path' => '/'],
-              ['moduleName' => 'Bank Masters', 'path' => '/bank-master'],
-              ['moduleName' => 'Vendor Masters', 'path' => '/vendor'],
-              ['moduleName' => 'Work Order Entry', 'path' => '/workorder'],
-              ['moduleName' => 'Cheque Book Entry', 'path' => '/chequebook'],
+            [
+                'moduleName' => 'Masters',
+                'icon' => '/svg/home.svg',
+
+                'subModules' => [
+                    ['moduleName' => 'Charts of Account', 'path' => '/'],
+                    ['moduleName' => 'Bank Masters', 'path' => '/bank-master'],
+                    ['moduleName' => 'Vendor Masters', 'path' => '/vendor'],
+                    ['moduleName' => 'Work Order Entry', 'path' => '/workorder'],
+                    ['moduleName' => 'Cheque Book Entry', 'path' => '/chequebook'],
+                ],
             ],
-          ],
-          [
-            'moduleName' => 'Documentation & Record Keeping',
-            'icon' => '/svg/home.svg',
-            'path' => '/document-and-record',
-            'subModules' => [
-              ['moduleName' => 'Bank Masters', 'path' => '/'],
-              ['moduleName' => 'Vendor Masters', 'path' => '/vendor'],
-              ['moduleName' => 'Work Order Entry', 'path' => '/workorder'],
-              ['moduleName' => 'Cheque Book Entry', 'path' => '/chequebook'],
+            [
+                'moduleName' => 'Documentation & Record Keeping',
+                'icon' => '/svg/home.svg',
+                'path' => '/document-and-record',
+                'subModules' => [
+                    ['moduleName' => 'Bank Masters', 'path' => '/'],
+                    ['moduleName' => 'Vendor Masters', 'path' => '/vendor'],
+                    ['moduleName' => 'Work Order Entry', 'path' => '/workorder'],
+                    ['moduleName' => 'Cheque Book Entry', 'path' => '/chequebook'],
+                ],
             ],
-          ],
         ],
-      ];
+    ];
 
     public function render()
     {
