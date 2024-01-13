18314ab035ed2711936d946c78017a3e9191eff1
sanjivgit <sanjivkumarhzb1427@gmail.com>
2024-01-13 10:51:54 +0530

    Improvement in sidebar

--- resources/views/livewire/side-bar.blade.php
+++ resources/views/livewire/side-bar.blade.php
@@ -5,7 +5,7 @@
         </div>
 
 
-        <details x-data="{ open: false }" class="rounded overflow-hidden">
+        <details x-data="{ open: false, currentPath: window.location.pathname }" class="rounded overflow-hidden">
             <summary @click="open = !open" class="cursor-pointer p-2 flex items-center justify-center bg-[#12743B]">
                 <a href={{$sidebarLinks['path']}}>
                     <div class="bg-white h-[31px] w-[31px] rounded-lg flex justify-center p-1 mr-2">
@@ -13,12 +13,12 @@
                     </div>                                                                                                          
                 </a>
                 <span class="mr-2 text-[18px]">{{$sidebarLinks['moduleName']}}</span>
-                <img src="/svg/demo.svg" x-bind:class="{ 'transform rotate-180': open }" alt="">
+                <img src="/svg/white-arrow.svg" x-bind:class="{ 'transform rotate-180': open }" alt="">
             </summary>
 
 
             @foreach($sidebarLinks['subModules'] as $subModules)
-                    <details class="my-2" x-data="{ open: false, currentPath: window.location.pathname, subPath: '{{$subModules['path']}}' }">
+                    <details class="my-2" x-data="{ open: false, subPath: '{{$subModules['path']}}' }">
                         <summary @click="open = !open" class="mx-3 cursor-pointer p-2 flex items-center justify-between" x-bind:class="{'bg-[#12743B] rounded text-white': currentPath === subPath }">
                             <a href={{$subModules['path']}} class="flex items-center">
                                 <div class="bg-[#12743B] h-[31px] w-[31px] rounded-lg flex justify-center p-1 mr-2">
@@ -26,7 +26,8 @@
                                 </div>                                                                                                          
                                 <span x-bind:class="{'text-white': currentPath === subPath }" class="mr-2 text-[#1F2733]">{{$subModules['moduleName']}}</span>
                             </a>
-                            <img src="/svg/demo.svg" x-bind:class="{ 'transform rotate-180': open }" alt="">
+                            <img x-show="currentPath === subPath" src="/svg/white-arrow.svg"  x-bind:class="{ 'transform rotate-180': open }" alt="">
+                            <img  x-show="currentPath !== subPath" src="/svg/gray-arrow.svg"  x-bind:class="{ 'transform rotate-180': open }" alt="">
                         </summary>
                         @foreach($subModules['subModules'] as $subModule)
                         <div class="mx-7 flex items-center">
